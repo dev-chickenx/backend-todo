8ebb51a2fcc2d7ecd87931c6abba7291623e4bc6
KAITO Nakatsuji <k.nakatsuji.dev@gmail.com>
2024-10-06 07:53:37 +0000

    add: schemaの追加とAPIの内容変更

---
+++ api/routers/task.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+import api.schemas.task as task_schema
+
+router = APIRouter()
+
+
+@router.get("/tasks", response_model=list[task_schema.Task])
+async def list_tasks():
+    pass
+
+
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+    pass
+
+
+@router.put("/tasks/{task_id}")
+async def update_task():
+    pass
+
+
+@router.delete("/tasks/{task_id}")
+async def delete_task():
+    pass
